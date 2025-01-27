e26f8100162215fc6e24fa4330d58a1655cbd5f8
pankajshetake07 <140935206+pankajshetake07@users.noreply.github.com>
2025-01-27 23:26:22 +0530

    Inheritance in java

---
+++ Inheritance/src/apps/PersonDemo.java
@@ -0,0 +1,12 @@
+package apps;
+
+import office.utility.Person;
+
+public class PersonDemo {
+
+	public static void main(String[] args) {
+		Person p = new Person("pankaj",19,03,2002);
+		p.display();
+	}
+
+}
